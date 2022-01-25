1139d70a3ad0516207164eddb9a2ca4dadac77ee
Sherzod Mirsharipov <mirsharipovsherzod@gmail.com>
2022-01-24 22:25:22 -0500

    fix env vars

--- db/database.js
+++ db/database.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize');
 
+//'postgres://postgres:postgres@localhost:5432/intouch_db'
 
-const db = new Sequelize('postgres://postgres:postgres@localhost:5432/intouch_db',
+const db = new Sequelize(process.env.DATABASE_URL + "?sslmode=no-verify",
     {logging : false}
 );
 
